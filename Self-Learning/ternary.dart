3433fcaadac4f5f47ba275010b074ca7ec529a21
rogersruga <rogersruga.g@gmail.com>
2025-06-23 18:15:45 +0300

    Ternary Operator

---
+++ Self-Learning/ternary.dart
@@ -0,0 +1,6 @@
+void main(){
+  int age = 24;
+
+  //Ternary operator
+  age >= 13 && age <= 19 ? print("You are a teenager") : print("You are not a teenager");
+}
---
+++ Self-Learning/voter.dart
@@ -0,0 +1,8 @@
+void main(){
+
+  int age = 20;
+
+  String status = (age >= 8) ? "a voter" : "not a voter";
+
+  print("You are $status");
+}
