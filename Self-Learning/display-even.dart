09884985a1f61f6a48580bbcbe453f4df5122f5b
rogersruga <rogersruga.g@gmail.com>
2025-06-23 19:22:41 +0300

    For loops

---
+++ Self-Learning/display-even.dart
@@ -0,0 +1,9 @@
+void main(){  
+
+  for (int i = 50; i <= 100; i++){
+    //Check if i iss even
+    if(i % 2 == 0){
+      print(i);
+    }
+  }
+}
---
+++ Self-Learning/display-sum.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  int total = 0;
+  int n = 100;
+
+  for(int i = 1; i <= n; i++){
+  total = total + i;
+  }
+
+  print("Total is $total");
+}
