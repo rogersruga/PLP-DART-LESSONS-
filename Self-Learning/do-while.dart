3728bc8432ad900f8a145d857f3922aa3a6bc23c
rogersruga <rogersruga.g@gmail.com>
2025-06-23 21:34:22 +0300

    do-while

---
+++ Self-Learning/do-while.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  // Printing 1 to 10
+  int i = 1;
+  do {
+    print(i);
+    i++;
+  } while(i <= 10);
+
+print("================================");
+
+  // Printing 10 to 1
+  int a = 10;
+  do {
+    print(a);
+    a--;
+  } while(a >= 1);
+}
