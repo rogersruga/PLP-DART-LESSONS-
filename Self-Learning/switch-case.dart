07572b3257eb70832cf294ea62e2b3d0d9c69919
rogersruga <rogersruga.g@gmail.com>
2025-06-23 17:52:28 +0300

    switch case

---
+++ Self-Learning/switch-case.dart
@@ -0,0 +1,38 @@
+void main(){
+
+  int day = 13;
+
+  switch(day){
+    case 1:
+    print("Today is Monday");
+    break;
+
+    case 2:
+    print("Today is Tuesday");
+    break;
+
+    case 3:
+    print("Today is Wednesday");
+    break;
+
+    case 4:
+    print("Today is Thursday");
+    break;
+
+    case 5:
+    print("Today is Friday");
+    break;
+
+    case 6:
+    print("Today is Saturday");
+    break;
+
+    case 7:
+    print("Today is Sunday");
+    break;
+    
+    default:
+    print("Please enter a valid day");
+  }
+
+}
