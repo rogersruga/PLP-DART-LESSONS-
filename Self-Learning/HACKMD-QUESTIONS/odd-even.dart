669e6dbc9b2318c7b83202ee5d033f35ff866efc
rogersruga <rogersruga.g@gmail.com>
2025-07-02 12:49:57 +0300

    even and odd numbers

--- Self-Learning/HACKMD-QUESTIONS/odd-even.dart
+++ Self-Learning/HACKMD-QUESTIONS/odd-even.dart
@@ -4,6 +4,14 @@ import 'dart:io';
 
 void main(){
 
-  
+  print("Kindy enter a number: ");
+  int? number = int.parse(stdin.readLineSync()!);
+
+  if( number % 2 == 0){
+    print("$number is an even number");
+    
+  }else{
+    print("$number is an odd number");
+  }
 
 }
